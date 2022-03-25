7d0c43aa08e3089db742a1eca99e6d165f77c0df
Ashik-dot <98025597+Ashik-dot@users.noreply.github.com>
2022-03-25 09:37:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import {useEffect} from 'react';
+import SearchIcon from "./search.svg";
+import MovieCard from './MovieCard.jsx';
 
-function App() {
+
+const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7';
+
+const movie1={
+  Poster: "N/A",
+  Title: "The Amazing Spiderman T4 Premiere Special",
+  Type: "movie",
+  Year: "2012",
+  imdbID: "tt2233044",
+}
+const App =()=>{
+  const[movies,setmovies] = useState([]);
+  const[searchTerm,setSearchTerm]= useState('');
+
+    const searchMovies = async(title)=>{
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      // console.log(data.Search);
+      setmovies(data.Search);
+
+    }
+    useEffect(()=>{
+      searchMovies('Terminator');
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className='app'>
+        <h1>TamilRockers</h1>
+        <div className='search'>
+          <input 
+          placeholder='Search for movies'
+          value={searchTerm} 
+          onChange={(e)=>setSearchTerm(e.target.value)} />
+          <img 
+          src={SearchIcon} 
+          alt= 'search' 
+          onClick={()=>searchMovies(searchTerm)} />
+          </div>
+     
+       {
+         movies?.length>0
+         ?
+         (
+           <div className='container'>
+             {movies.map((movie) =>
+              (<MovieCard movie= {movie}/>))}
+         </div> )
+         :
+        (<div className='empty'>
+          <h2>No movies found</h2>
+        </div>)
+         
+       }
+   
+</div>
+   
   );
 }
 
